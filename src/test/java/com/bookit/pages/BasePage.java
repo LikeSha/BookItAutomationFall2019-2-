a10e18036327433aa0cc392d8dc74d3c09e0cab6
Like Sha <tradersalike88@yahoo.com>
2020-05-31 22:17:53 -0700

    updated BasePage class code

--- src/test/java/com/bookit/pages/BasePage.java
+++ src/test/java/com/bookit/pages/BasePage.java
@@ -2,6 +2,7 @@ package com.bookit.pages;
 
 import com.bookit.utilities.Driver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -18,7 +19,8 @@ public abstract class BasePage {
     @FindBy(linkText = "self")
     protected WebElement self;
 
-    protected WebDriverWait wait = new WebDriverWait((Driver.getDriver(),20));
+    protected WebDriverWait wait = new WebDriverWait(Driver.getDriver(),20);
+    protected Actions actions = new Actions(Driver.getDriver());
 
     public BasePage(){
         PageFactory.initElements(Driver.getDriver(),this);
@@ -26,8 +28,8 @@ public abstract class BasePage {
 
     public void goToSelfPage(){
         wait.until(ExpectedConditions.elementToBeClickable(my)).click();
-        wait.until(ExpectedConditions.elementToBeClickable(self)).click();
-        my.click();
+        actions.moveToElement(my).pause(2000).click(self).perform();
+
     }
 
 
