e7defaf0f5f141cf8e5f105703a9ce55ffd67e24
Like Sha <tradersalike88@yahoo.com>
2020-12-02 10:44:59 -0800

    updated CucumberRunner classnote code

--- src/test/java/com/bookit/runners/CucumberRunner.java
+++ src/test/java/com/bookit/runners/CucumberRunner.java
@@ -6,13 +6,14 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        glue = "com/bookit/step_definitions",
-        features = "src/test/resources/features",
+        glue = "com/bookit/step_definitions", // glue need to connect with step_definitions
+        features = "src/test/resources/features",// features specify feature files (copy content root)
         plugin = {
                 "json:target/cucumber.json"
-        },
-        tags = "@create_student_3",
-        dryRun = false
+        }, // plugin for generating json report.
+        tags = "@create_student_3",// tags to specify which scenario to run which to be ignored
+        dryRun = false // dry run its test run, not execution ,it used to ensure that every step definition
+                       // has code implementation
 
 )
 public class CucumberRunner {
