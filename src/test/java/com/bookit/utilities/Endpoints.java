a6c9d9a7c9c5d72a3826ab40d5b5267af1d3c77c
Like Sha <tradersalike88@yahoo.com>
2020-12-06 17:52:05 -0800

    updated Endpoints classnote code

--- src/test/java/com/bookit/utilities/Endpoints.java
+++ src/test/java/com/bookit/utilities/Endpoints.java
@@ -7,4 +7,7 @@ public class Endpoints {
     public static String GET_ME = "/api/students/me";
     public static String ADD_BATCH = "/api/batches/batch";
     public static String ADD_TEAM = "/api/teams/team";
+
+    // if those String variables are not static ,we can not call them without creating object of this class!
+    // so we MUST make them static ! so we can direct use them. just use class name.variable name
 }
